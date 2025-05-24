97eae0e7fe92eb1b92799c6ef3d3019002b8643d
Yevhenii Dehtiar <yevhenii.dehtiar@gmail.com>
2025-05-25 01:28:39 +0200

    Update database path and entrypoint location in setup
    Adjusted the database and static file paths to use a dedicated directory under `/var/lib/django` for better organization. Relocated and updated the entrypoint script to reside in `/usr/local/bin` for improved accessibility in PATH.

--- django-code-smells/django_code_smells/django_code_smells/settings.py
+++ django-code-smells/django_code_smells/django_code_smells/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = "django_code_smells.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": Path("/var/lib/django/db.sqlite3"),
     }
 }
 
@@ -128,6 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = "/var/lib/django/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
